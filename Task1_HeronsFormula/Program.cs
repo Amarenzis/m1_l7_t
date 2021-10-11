d21692f924a3d9b8d9ed3f7aa933049c1c644685
Darya Anishchenko <Darya.Anishchenko@bonava.com>
2021-10-11 23:23:52 +0300

    Добавьте файлы проекта.

---
+++ Task1_HeronsFormula/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1_HeronsFormula
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Ввод данных
+            Console.WriteLine("Введите первую сторону первого треугольника");
+            double a1Side = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите вторую сторону первого треугольника");
+            double b1Side = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите третью сторону первого треугольника");
+            double c1Side = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Введите первую сторону второго треугольника");
+            double a2Side = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите вторую сторону второго треугольника");
+            double b2Side = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите третью сторону второго треугольника");
+            double c2Side = Convert.ToDouble(Console.ReadLine());
+            #endregion
+
+            double area1Triangle = Heron(a1Side, b1Side, c1Side);
+            double area2Triangle = Heron(a2Side, b2Side, c2Side);
+
+            if (area1Triangle>area2Triangle)
+            {
+                Console.WriteLine("Площадь первого треугольника больше площади второго {0:f2} > {1:f2}",area1Triangle,area2Triangle);
+            }
+            else
+            {
+                Console.WriteLine("Площадь первого треугольника меньше площади второго {0:f2} < {1:f2}", area1Triangle, area2Triangle);
+            }
+            Console.ReadKey();
+        }
+        static double Heron (double a, double b, double c)
+        {
+            double perimeterHalf = (a + b + c) / 2;
+            double area = Math.Sqrt(perimeterHalf*(perimeterHalf-a)*(perimeterHalf-b)*(perimeterHalf-c));
+            return area;
+        }
+    }
+}
---
+++ Task2_CubeVolumeAndArea/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2_CubeVolumeAndArea
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Внесите значение длины ребра куба:");
+            double sideCube = Convert.ToDouble(Console.ReadLine());
+
+            double areaCube = 0.00;
+            double volumeCube = 0.00;
+
+            CubeCalcs(sideCube, out areaCube, out volumeCube);
+            Console.WriteLine("Площадь всех поверхностей куба составляет {0:f2}, а объём куба {1:f2}", areaCube,volumeCube);
+            Console.ReadKey();
+        }
+        static void CubeCalcs (double a, out double S, out double V)
+        {
+            S = a * a * 6;
+            V = Math.Pow(a, 3.00);
+        }
+    }
+}
